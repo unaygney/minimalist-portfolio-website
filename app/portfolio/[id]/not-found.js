1519c0814651a6f71406936ebd4ab57e441788a8
Guney Unay <guneymarketing17@hotmail.com>
2023-12-23 19:31:42 +0300

    minor changes

--- app/portfolio/[id]/not-found.js
+++ app/portfolio/[id]/not-found.js
@@ -6,7 +6,6 @@ function notFound() {
     <main className="flex items-center justify-center  py-10  px-12 ">
       <section className="flex flex-col">
         <p className="body-text-1">
-          {" "}
           It looks like you tried to access a page that doesn't exist.
         </p>
         <NotFound />
